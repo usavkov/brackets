b81291caf6f5160d089b4c8e99047111965d706c
Yuri Savkov <yury.saukou@gmail.com>
2020-12-19 22:00:59 +0300

    add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = []
+  let strcopy = str;
+
+  for (let el of bracketsConfig) {
+    if (parseInt(str)) {
+      arr.push(`(${el[0]}${el[1]})`);
+    } else {
+      arr.push('(\\' + el[0] + '\\' + el[1] + ')');
+    }
+  }
+
+  const pattern = arr.join('|');
+
+  const re = new RegExp(pattern, 'g');
+  
+  const lenarr = [-1];
+  lenarr.push(str.length);
+  let i = 1;
+  let curlen = 0;
+
+ while (lenarr[i-1] !== lenarr[i]) {
+    strcopy = strcopy.replace(re, '');
+    curlen = strcopy.length
+    lenarr.push(curlen)
+    i++;
+  }
+
+  return !strcopy
 }
